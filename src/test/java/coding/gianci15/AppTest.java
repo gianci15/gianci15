314f0a5c8386a06f54cae125b6434c01760fb237
Giovanny <gianci15@ingenieros.com>
2013-10-01 23:09:59 -0500

    Primer Commit

---
+++ src/test/java/coding/gianci15/AppTest.java
@@ -0,0 +1,19 @@
+package coding.gianci15;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AppTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		assert(true);
+	}
+
+}
