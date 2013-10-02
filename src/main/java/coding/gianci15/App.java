676c43bb22f41e49fe0554de7241798c5c0c13ae
gianci15 <gianci15@ingenieros.com>
2013-10-02 00:10:43 -0500

    Hola Mundo Maven

--- src/main/java/coding/gianci15/App.java
+++ src/main/java/coding/gianci15/App.java
@@ -1,13 +1,41 @@
 package coding.gianci15;
 
+import javax.swing.JOptionPane;
+
 /**
- * Hello world!
+ * Hello world! in maven  comando : clean compile exec:java
+ * y añadir al pom 
+ 
+ <build>
+        <plugins>
+            <plugin>
+                <groupId>org.codehaus.mojo</groupId>
+                <artifactId>exec-maven-plugin</artifactId>
+                <version>1.2.1</version>
+                <executions>
+                    <execution>
+                        <goals>
+                            <goal>java</goal>
+                        </goals>
+                    </execution>
+                </executions>
+                <configuration>
+                    <mainClass>coding.gianci15.App</mainClass>
+                    <arguments>
+                        <argument>Andrés</argument>
+                    </arguments>
+                </configuration>
+            </plugin>
+        </plugins>
+    </build>
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	 public static void main( String[] args )
+	    {
+	       JOptionPane.showMessageDialog(null,"Hola Mundo");
+ 
+	    }
+
 }
